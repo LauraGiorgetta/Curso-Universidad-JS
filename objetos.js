f3e9088870c6c15ff35bcc6e37355c800330976f
= <laura.gisela.g@gmail.com>
2021-06-17 01:00:28 -0300

    Trabajando con get y set

--- objetos.js
+++ objetos.js
@@ -4,19 +4,24 @@ persona = {
     mail: 'laura.gisela.g@gmail.com',
     edad: 25,
     idioma: 'es',
-    nombreCompleto : function(){ //arrow no tiene this
+    nombreCompleto: function () { //arrow no tiene this
         return this.nombre + ' ' + this.apellido;
     },
-    get nombreCompleto2(){ //method get -> se define una funcion y se llama como si fuera atributo
+    get nombreCompleto2() { //method get -> se define una funcion y se llama como si fuera atributo
         return this.nombre + ' ' + this.apellido + ' :D';
     },
-    get lang(){ //Get no funciona sin return
-        return lang.toUpperCase();
+
+    get lang() { //Get no funciona sin return
+        return this.idioma.toUpperCase();
     },
-    set lang(lang){ //Set no funciona si no tiene al menos 1 parametro
+    set lang(lang) { //Set no funciona si no tiene al menos 1 parametro
         this.idioma = lang.toUpperCase();
     }
 }
 
 console.log(persona.nombreCompleto());
 console.log(persona.nombreCompleto2);
+
+console.log(persona.lang);
+persona.lang = 'spa';
+console.log(persona.idioma);
